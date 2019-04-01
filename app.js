caa4e2d22f84710f046534adb1fd009f20b9fbef
June Hyok Cho <jcho0627@gmail.com>
2019-04-01 12:50:37 +0200

    express complete (check required)

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ let speak = {
   dog: "Woof Woof!"
 }
 
-
 app.get("/", (req, res) => res.send("Hi there, welcome to my assignment!"));
 
 app.get("/speak/:animal", function (req, res) {
-  let params = req.params.animal;
-  res.send(`The ${params} says ${speak[params]}`);
+  let animal = req.params.animal;
+  res.send(`The ${animal} says ${speak[animal]}`);
 });
 
 app.get("/repeat/:str/:number", function (req, res) {
   let str = req.params.str;
-  let params = req.params.number;
-  let num = Number(params);
-  res.send(function (num) {
-    for (let i = 0; i < num; i++) {
-      console.log("str");
-    }
-  });
+  let num = Number(req.params.number);
+  let result = [];
+
+  for (let i = 0; i < num; i++) {
+    result.push(str);
+  }
+
+  result = result.join(" ");
+
+  res.send(result);
 });
 
 app.get("*", (req, res) => res.send("Sorry, page not found...What are you doing with your life?"));
-
 
 app.listen(port, () => console.log(`Server has Started on port ${port}!`));
